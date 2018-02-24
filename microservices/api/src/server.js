d89cb892c9f2aee6ce03e9e8c15f509be3f2a201
unknown <you@example.com>
2018-02-24 16:55:38 +0530

    added the backend file

--- microservices/api/src/server.js
+++ microservices/api/src/server.js
@@ -1,24 +1,54 @@
-var express = require('express');
-var app = express();
-var request = require('request');
-var router = express.Router();
-var morgan = require('morgan');
-var bodyParser = require('body-parser');
-require('request-debug')(request);
+var express=require("express")
+var app=express()
+const https=require("https");
+const bodyParser = require('body-parser')
 
-var hasuraExamplesRouter = require('./hasuraExamples');
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+var router = express.Router();
 
 var server = require('http').Server(app);
 
-router.use(morgan('dev'));
+var url="https://api.fixer.io/latest?base="
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
 
-app.use('/', hasuraExamplesRouter);
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+app.post("/webhook",function(req,res){
+ 
+  	
+		
+	var t=req.body.result.parameters.t[0]; 
+	var a=req.body.result.parameters.f[0]["amount"];
+	var f=req.body.result.parameters.f[0]["currency"];
+		
+	
+	var exchange_rate;
+	https.get(url+f,resp=>
+		{
+			let data=""
+			resp.on("data",(chunk)=>
+			{
+				data+=chunk;
+			});
+			resp.on("end",()=>
+			{
+				
+				r=(data.rates[t]);
+				var c=a;
+				var output=" "+amount+" " +convert_from+" equals "+c+" "+convert_to;
+				
+				         res.setHeader('Content-Type', 'application/json');
+				         res.send(JSON.stringify({ 'speech': output, 'displayText': output }));
+				
+			});
+
+		}).on("error",err=>{
+			var error="error"+err.message;	
+			// If there is an error let the user know
+			     res.setHeader('Content-Type', 'application/json');
+			    res.send(JSON.stringify({ 'speech': error, 'displayText': error }));
+		});
+	
 });
+app.listen(8080,function(){
+console.log("Server is running")});
